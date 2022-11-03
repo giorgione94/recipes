31af4881dcd1d3248e0d2f83adfe5504c8e8da8b
Giorgio Biancani <giorgio.biancani@gmail.com>
2022-11-03 17:11:13 +0100

    Footer updated

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="bg-dark bg-dark-gradient">
         <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xxl-4 g-4">
             <div class="col mb-2">
-                <h4 class="text-light font-monospace"> &nbsp;Follow Us</h4>
+                <h4 class="text-light font-monospace">&nbsp;Follow Us</h4>
                 <div class="mb-1">
                     &nbsp; &nbsp;<i class="bi bi-facebook text-light font-monospace">&nbsp;Facebook</i>
                 </div>
@@ -17,37 +17,37 @@
                 </div>
             </div>
             <div class="col mb-2">
-                <h4 class="text-light font-monospace">Info</h4>
+                <h4 class="text-light font-monospace">&nbsp;Info</h4>
                 <div class="mb-1">
-                    <p class="text-light font-monospace">Chi siamo</p>
+                    <p class="text-light font-monospace">&nbsp;Chi siamo</p>
                 </div>
                 <div class="mb-1">
-                    <p class="text-light font-monospace">Accedi</p>
+                    <p class="text-light font-monospace">&nbsp;Accedi</p>
                 </div>
                 <div class="mb-1">
-                    <p class="text-light font-monospace">Abbonati</p>
+                    <p class="text-light font-monospace">&nbsp;Abbonati</p>
                 </div>
                 <div class="mb-1">
-                    <p class="text-light font-monospace">Gruppo Mondadori</p>
-                </div>
-            </div>
-            <div class="col mb-2">
-                <h4 class="text-light font-monospace">Discover</h4>
-                <div class="mb-1">
-                    <p class="text-light font-monospace">Crea un blog</p>
-                </div>
-                <div class="mb-1">
-                    <p class="text-light font-monospace">In edicola</p>
-                </div>
-                <div class="mb-1">
-                    <p class="text-light font-monospace">Shopping</p>
-                </div>
-                <div class="mb-1">
-                    <p class="text-light font-monospace">Ultime ricette</p>
+                    <p class="text-light font-monospace">&nbsp;Gruppo Mondadori</p>
                 </div>
             </div>
             <div class="col mb-2">
-                <h4 class="text-light font-monospace">Download App</h4>
+                <h4 class="text-light font-monospace">&nbsp;Discover</h4>
+                <div class="mb-1">
+                    <p class="text-light font-monospace">&nbsp;Crea un blog</p>
+                </div>
+                <div class="mb-1">
+                    <p class="text-light font-monospace">&nbsp;In edicola</p>
+                </div>
+                <div class="mb-1">
+                    <p class="text-light font-monospace">&nbsp;Shopping</p>
+                </div>
+                <div class="mb-1">
+                    <p class="text-light font-monospace">&nbsp;Ultime ricette</p>
+                </div>
+            </div>
+            <div class="col mb-2">
+                <h4 class="text-light font-monospace">&nbsp;Download App</h4>
                 <div class="col">
                     <i class="bi bi-apple btn btn-dark text-light font-monospace">&nbsp;App Store</i>
                     <br>
